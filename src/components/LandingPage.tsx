6a5f5ea3b0936a1dd6fcfe4f37153018d2a4f85a
Martin Slabý <martins@thinkeasy.cz>
2022-03-28 17:01:47 +0200

    landing

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -3,6 +3,24 @@ import React from "react";
 const LadingPage = () => {
   return (
     <div className="d-flex justify-content-end">
+      <div className="inner">
+        <div className="circle">
+          <div>+</div>
+          <span>Jediná společnost svého druhu v ČR</span>
+        </div>
+        <div className="circle">
+          <div>+</div>
+          <span>Realizace projektů u nás i v zahraničí</span>
+        </div>
+        <div className="circle">
+          <div>+</div>
+          <span>Osobní přístup, empatie, porozumění</span>
+        </div>
+      </div>
+      <div className="section-left overflow-hidden d-flex align-items-end flex-column">
+        <div className="skewed1" />
+        <div className="skewed2" />
+      </div>
       <div className="section-right">
         <div className="landingPageFilter1">
           <h1 className="heading">BRZY NASHLEDANOU</h1>
